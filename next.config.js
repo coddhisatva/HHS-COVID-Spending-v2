1fc1df8fc86dd353b730b3b20bc295f13a7b61d1
skaymore <conoregan151@gmail.com>
2025-03-24 09:54:14 -0400

    Optimizations

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  // Improve development performance
+  swcMinify: true,
+  
+  // Reduce memory usage during builds
+  onDemandEntries: {
+    // Period (in ms) where the server will keep pages in the buffer
+    maxInactiveAge: 15 * 1000,
+    // Number of pages that should be kept simultaneously without being disposed
+    pagesBufferLength: 2,
+  },
+  
+  // Disable unused features to save memory
+  images: {
+    disableStaticImages: false, // Set to true if you don't use static images
+  },
+}
 
-module.exports = nextConfig; 
+module.exports = nextConfig 
